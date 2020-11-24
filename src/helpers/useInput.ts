7b1779322f924731c50961d22fda31101d8aa028
Yura <Yura.DNG@gmail.com>
2020-11-24 21:08:21 +0200

    custom hook added

--- src/helpers/useInput.ts
+++ src/helpers/useInput.ts
@@ -6,28 +6,45 @@ export type ValidType = {
 
 export const useInput = (initValue: any, validations?: ValidType[] ) => {
   const [value, setValue] = useState(initValue)
-  const [isFocused, setFocused] = useState(false)
-  const [errors, setErrors] = useState("")
+  const [wasFocused, setFocused] = useState(false)
+  const [error, setError] = useState("")
+  const [style, setStyle] = useState({})
 
   useEffect(() => {
-    validations?.map(validator => {
-      setErrors(validator(value))
-    })
-  }, [value])
+    if (wasFocused) {
+      validations?.map(validator => {
+        setError(validator(value))
+      })
+    }
+  }, [value, wasFocused])
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    if (error.length !== 0) {
+      setStyle({
+        borderColor: "red",
+        borderWidth: "2px",
+        borderStyle: "solid"
+      })
+    } else {
+      setStyle({
+        borderColor: "white"
+      })
+    }
+  }, [error])
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     setValue(event.target.value)
   }
 
-  const onBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFocused(true)
+  const onBlur = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setFocused(true) 
   }
 
   return {
     value,
-    errors,
+    error,
     onChange,
     onBlur,
-    isFocused
+    style
   }
 }
